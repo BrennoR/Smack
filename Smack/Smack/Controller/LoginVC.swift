31825e54c0bc594d0d9463bee263d297c2bd14fc
Brenno Ribeiro <brenno.brenno.ribeiro.ribeiro@gmail.com>
2018-02-24 01:58:08 -0500

    Lesson 04

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Brenno Ribeiro on 2/24/18.
+//  Copyright © 2018 Brenno Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Brenno Ribeiro on 2/24/18.
+//  Copyright © 2018 Brenno Ribeiro. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
