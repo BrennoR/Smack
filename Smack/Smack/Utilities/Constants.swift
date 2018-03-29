0de2bd8bc57013291d5ddcf53c89433f18f1c091
Brenno Ribeiro <brenno.brenno.ribeiro.ribeiro@gmail.com>
2018-03-28 23:53:53 -0400

    Lesson 12)

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -14,6 +14,8 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 let BASE_URL = "https://smackchatapplesson.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
 
 // Segues
 
@@ -26,3 +28,11 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+
+let HEADER = ["Content-Type": "application/json; charset=utf-8"]
+
+
+
+
