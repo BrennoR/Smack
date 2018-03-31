e277fe83354f13c7e28af471ffde2bea9144141b
Brenno Ribeiro <brenno.brenno.ribeiro.ribeiro@gmail.com>
2018-03-30 22:51:06 -0400

    Login Avatar BGColor

--- Smack/Smack/Services/UserDataService.swift
+++ Smack/Smack/Services/UserDataService.swift
@@ -30,7 +30,37 @@ class UserDataService {
         self.avatarName = avatarName
     }
     
-    
+    func returnUIColor(component: String) -> UIColor {
+//        "[0.803921568627451, 0.968627450980392, 0.458823529411765, 1]",
+        
+        let scanner = Scanner(string: component)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a : NSString?
+        
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        guard let rUnwrapped = r else { return defaultColor }
+        guard let gUnwrapped = g else { return defaultColor }
+        guard let bUnwrapped = b else { return defaultColor }
+        guard let aUnwrapped = a else { return defaultColor }
+        
+        let rFloat = CGFloat(rUnwrapped.doubleValue)
+        let gFloat = CGFloat(gUnwrapped.doubleValue)
+        let bFloat = CGFloat(bUnwrapped.doubleValue)
+        let aFloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rFloat, green: gFloat, blue: bFloat, alpha: aFloat)
+        
+        return newUIColor
+    }
     
     
     
